434095c6add2b320e0686c080e2ba3c9f749d7b4
inh24 <ngodiecpham0627@gmail.com>
2021-07-07 21:24:52 +0700

    demobill

---
+++ Tho_Real/src/tho_real/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tho_real;
+
+import swingui.LoginSignUpForm;
+
+/**
+ *
+ * @author Dell
+ */
+public class Main {
+    public static void main(String[] args) {
+        LoginSignUpForm  dangnhapframe = new LoginSignUpForm ();
+        dangnhapframe.setVisible(true);
+    }
+}
